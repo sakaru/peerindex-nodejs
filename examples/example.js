4768d741bd8a8314cc980587beecb5ed28ec6cb1
Sid Karunaratne <sk@peerindex.com>
2013-02-27 17:25:35 +0000

    Replaced Ferenc with Mischa as the example user since Mischa returns a better social graph. Sorry Ferenc

--- examples/example.js
+++ examples/example.js
@@ -3,7 +3,7 @@ var PeerIndexClient = require('../lib/peerindex.js');
 var api = new PeerIndexClient('f183qza9k88mjynhtjfc56ce');
 
 var query = {
-    twitter_screen_name: 'fhuszar'
+    twitter_screen_name: 'mischatuffield'
 };
 var basic = api.actorBasic(query, function (actor, result) {
     console.log("Status code: " + result.statusCode, actor);
