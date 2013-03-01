6b43dcb55648b96dc03abf4cc523e8ade1d028ca
Sid Karunaratne <sk@peerindex.com>
2013-03-01 19:08:24 +0000

    Change example file to do an extended search instead of a basic search Pretty print the resulting json

--- examples/example.js
+++ examples/example.js
@@ -5,6 +5,7 @@ var api = new PeerIndexClient('f183qza9k88mjynhtjfc56ce');
 var query = {
     twitter_screen_name: 'mischatuffield'
 };
-var basic = api.actorBasic(query, function (actor, result) {
-    console.log("Status code: " + result.statusCode, actor);
+var basic = api.actorExtended(query, function (actor, result) {
+    console.log("Status code: " + result.statusCode);
+    console.log(JSON.stringify(actor, null, 2));
 });
